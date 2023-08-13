c5de93d8d1579f1c1801fa99dc9a6532f2a20c58
gzq <3454391157@qq.com>
2023-08-13 15:00:36 +0800

    version 04

--- src/main/java/com/gzq/test/Test.java
+++ src/main/java/com/gzq/test/Test.java
@@ -4,5 +4,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("004");
         System.out.println("003");
+        System.out.println("004");
     }
 }
